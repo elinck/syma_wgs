8b95833ea6d839d73d982e8bdcbfe09ef04e1980
elinck <elinck@uw.edu>
2018-04-06 15:46:07 -0700

    spacing

--- mitosemblr.R
+++ mitosemblr.R
@@ -7,11 +7,12 @@ blastR <- function(read_dir, reference){
   commands <- c()
   for(i in 1:20){
     system('echo "$@"_starting blatq search to pull seeds for PRICE')
-    commands[i] <- paste0("blatq -t=dna", reference, R1[i], 
+    commands[i] <- paste0("blatq -t=dna ", 
+                          reference , R1[i] , 
                           R1[i] %>% basename() %>% strsplit("_L006_R._001") %>% unlist() %>% .[1],"R1_matches.m8;", 
-                          "blatq -t=dna", reference, R2[i], 
+                          "blatq -t=dna ", reference , R2[i] , 
                           R2[i] %>% basename() %>% strsplit("_L006_R._001") %>% unlist() %>% .[1],"R2_matches.m8;", 
-                          "blatq -t=dna", reference, ST[i],
+                          "blatq -t=dna ", reference , ST[i] ,
                           ST[i] %>% basename() %>% strsplit("_L006_R._001") %>% unlist() %>% .[1],"singleton_matches.m8")
     system('echo "$@" Blatq done')
   }
@@ -30,11 +31,11 @@ excerptR <- function(read_dir, m8_dir, excerptByIDPath){
   commands <- c()
   for(i in 1:20){
     system('echo "$@"Now starting to excerpt reads with blatq hits by ID')
-    commands[i] <- paste0(excerptByIDPath, R1[i], M1[i], ">", 
+    commands[i] <- paste0(excerptByIDPath , R1[i] , M1[i] , "> ", 
                           R1[i] %>% basename() %>% strsplit("R1_matches.m8") %>% unlist() %>% .[1],"R1_matches.fq;",
-                          excerptByIDPath, R2[i], M2[i], ">", 
+                          excerptByIDPath , R2[i] , M2[i] , "> ", 
                           R2[i] %>% basename() %>% strsplit("R2_matches.m8") %>% unlist() %>% .[1],"R2_matches.fq;",
-                          excerptByIDPath, ST[i], MS[i], ">", 
+                          excerptByIDPath , ST[i] , MS[i] , ">", 
                           ST[i] %>% basename() %>% strsplit("singleton_matches.m8") %>% unlist() %>% .[1],"singleton_matches.fq;")
   }
     system('"$@"done Excerpting reads...')
@@ -48,7 +49,7 @@ seedR <- function(read_dir){
   R2 <- list.files(read_dir, full.names=T) %>% grep("R2_matches.fq",.,value=T)
   ST <- list.files(read_dir, full.names=T) %>% grep("singleton_matches.fq",.,value=T)
   for(i in 1:20){
-    commands[i] <- paste0('cat', R1[i], R2[i], ST[i], '>', 
+    commands[i] <- paste0('cat ', R1[i] , R2[i] , ST[i] , '> ', 
                           R1[i] %>% basename() %>% strsplit("R1_matches.m8") %>% unlist() %>% .[1],'SEEDS.fastq')
   }
   for(i in commands){
@@ -60,7 +61,7 @@ SPAdeR <- function(read_dir, spades_path){
   seeds <- list.files(read_dir, full.names=T) %>% grep("SEEDS.fastq",.,value=T)
   system('echo "$@"Now starting SPAdes assembly...')
   for(i in 1:20){
-    commands[i] <- paste0('python', spades_path, '--careful -s', seeds[i], 
+    commands[i] <- paste0('python ', spades_path , '--careful -s ', seeds[i] , 
                           seeds[i] %>% basename() %>% strsplit("SEEDS.m8") %>% unlist() %>% .[1],'SEEDS_assembled')
   }
 }
@@ -73,11 +74,11 @@ priceR <- function(read_dir, price_path, insertSize, identityPercentage, cycles,
   seeds <- list.files(read_dir, full.names=T) %>% grep("SEEDS_assembled",.,value=T)
   system('echo "$@"Now starting PRICE assembly using reads')
   for(i in 1:20){
-    commands[i] <- paste(price_path, '-fpp', R1[i], R2[i], insertSize, identityPercentage,
-                         '-icf', seeds, '1 1 5', '-nc', cycles, '-mol', mol, '-mpi', mpi,
-                         '-MPI', MPI, '-a', '-a 8 -target 85 1 1 1 -o', 
-                         seeds[i] %>% basename() %>% strsplit("SEEDS_assembled") %>% unlist() %>% .[1],'mtDNA.fasta', '-o',
-                         seeds[i] %>% basename() %>% strsplit("SEEDS_assembled") %>% unlist() %>% .[1],'mtDNA.fasta',
-                         '-maxHp 25 -logf', logpath)
+    commands[i] <- paste(price_path , '-fpp ', R1[i] , R2[i] , insertSize , identityPercentage ,
+                         '-icf ', seeds , '1 1 5 ', '-nc ', cycles , '-mol ', mol , '-mpi ', mpi ,
+                         '-MPI ', MPI , '-a ', '-a 8 -target 85 1 1 1 -o ', 
+                         seeds[i] %>% basename() %>% strsplit("SEEDS_assembled") %>% unlist() %>% .[1],'mtDNA.fasta ', '-o ',
+                         seeds[i] %>% basename() %>% strsplit("SEEDS_assembled") %>% unlist() %>% .[1],'mtDNA.fasta ',
+                         '-maxHp 25 -logf ', logpath)
   }
 }
