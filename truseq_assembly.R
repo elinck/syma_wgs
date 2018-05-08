1e55912d56f20529a806427b3c0defb6ca8be2b9
elinck <elinck@uw.edu>
2018-05-08 15:08:11 -0700

    overhaul for bbmap

--- truseq_assembly.R
+++ truseq_assembly.R
@@ -1,41 +1,52 @@
 ############################################################
 # truseq resequencing SNP calling pipeline                 #
-# modified by E. Linck, original wrappers by C.J. Battey   #
+# modified by E. Linck, wrappers inspired by C.J. Battey   #
 ############################################################
 
-setwd("/data/jdumbacher/Syma/syma_wgs/")
+setwd("/media/burke/bigMac/ethan/")
 install.packages("magrittr", repo = "http://ftp.osuosl.org/pub/cran/"); install.packages("foreach", repo = "http://ftp.osuosl.org/pub/cran/"); install.packages("doMC", repo = "http://ftp.osuosl.org/pub/cran/")
 library(magrittr);library(foreach);library(doMC)
 registerDoMC(cores=4)
 
-#######################################################################################
-# setup: you'll need R, java (v1.8), samtools, AdapterRemoval; bbmap; NextGenMap;     #
-# and angsd                                                                           #
-# Budget around 5x the initial download size for hard drive space                     #
-#######################################################################################
+# setup: you'll need R, java (v1.8), samtools, bbmap
 
-#run AdapterRemoval to trim adapters, merge overlapping paired reads, and drop low-quality base calls#
-R1 <- list.files("/data/jdumbacher/Syma/syma_wgs/raw",full.names=T) %>% grep("R1_001",.,value=T)
-R2 <- list.files("/data/jdumbacher/Syma/syma_wgs/raw",full.names=T) %>% grep("R2_001",.,value=T)
+# mask repeats from reference using bbmask entropy approach
+system("bbmask.sh in=B10K-DU-024-03.genomic.fa out=
+B10K-DU-024-03.genomic.masked.fa entropy=0.7")
+
+#run bbduk to trim adapters
+R1 <- list.files("/media/burke/bigMac/ethan/syma_raw",full.names=T) %>% grep("R1_001",.,value=T)
+R2 <- list.files("/media/burke/bigMac/ethan/syma_raw",full.names=T) %>% grep("R2_001",.,value=T)
 commands <- c()
 for(i in 1:20){
-  commands[i] <- paste0("AdapterRemoval --file1 ", R1[i],
-                        " --file2 ",R2[i],
-                        " --basename trimmed/", R1[i] %>% basename() %>% strsplit("_L006_R._001") %>% unlist() %>% .[1],
-                        " --trimns --trimqualities --collapse --threads 32")
+  commands[i] <- paste0("bbduk.sh in1=", R1[i],
+                        " in2=",R2[i],
+                        " out=", R1[i] %>% basename() %>% strsplit("_L006_R._001") %>% unlist() %>% .[1],"trimmed.fq",
+                        " ref=adapters.fa ktrim=r k=21 mink=11 hdist=2 tpe tbo")
 }
-
 for(i in commands){
   system(i)
 }
-#foreach(i=commands) %dopar% system(i) #untested parallel version
+system("mkdir syma_trimmed; mv *trimmed* syma_trimmed")
 
-################################################################
-# remove duplicate reads (bbmap::dedupe.sh; memory intensive   #
-################################################################
+#run bbduk to quality trim
+R1 <- list.files("/data/jdumbacher/Syma/syma_wgs/syma_trimmed",full.names=T) %>% grep("R1_001",.,value=T)
+R2 <- list.files("/data/jdumbacher/Syma/syma_wgs/syma_trimmed",full.names=T) %>% grep("R2_001",.,value=T)
+commands <- c()
+for(i in 1:20){
+  commands[i] <- paste0("bbduk.sh in1=", R1[i],
+                        " in2=",R2[i],
+                        " out=", R1[i] %>% basename() %>% strsplit("_L006_R._001") %>% unlist() %>% .[1],"clean.fq",
+                        " qtrim=r trimq=10")
+}
+for(i in commands){
+  system(i)
+}
+system("mkdir syma_cleaned; mv *clean* syma_cleaned")
 
-R1 <- list.files("/data/jdumbacher/Syma/syma_wgs/trimmed",full.names=T) %>% grep("pair1",.,value=T)
-R2 <- list.files("/data/jdumbacher/Syma/syma_wgs/trimmed",full.names=T) %>% grep("pair2",.,value=T)
+# remove duplicate reads
+R1 <- list.files("/data/jdumbacher/Syma/syma_wgs/syma_cleaned",full.names=T) %>% grep("pair1",.,value=T)
+R2 <- list.files("/data/jdumbacher/Syma/syma_wgs/syma_cleaned",full.names=T) %>% grep("pair2",.,value=T)
 commands <- c()
 for(i in 1:20){
   sampleID[i] <- basename(R1[i]) %>% strsplit("_S...pair1.truncated") %>% unlist() %>% .[1]
@@ -53,27 +64,29 @@ for(i in 1:20){
   system(commands[i])
 }
 
-system("mkdir dedup; mv *dedup* dedup")
+system("mkdir syma_dedup; mv *dedup* syma_dedup")
 
-################################################################
-# align trimmed reads to reference sample                      #
-################################################################
-R1 <- list.files("/data/jdumbacher/Syma/syma_wgs/dedup",full.names=T) %>% grep("R1.dedup",.,value=T)
-R2 <- list.files("/data/jdumbacher/Syma/syma_wgs/dedup",full.names=T) %>% grep("R2.dedup",.,value=T)
+# align trimmed reads to reference sample
+R1 <- list.files("/data/jdumbacher/Syma/syma_wgs/syma_dedup",full.names=T) %>% grep("R1.dedup",.,value=T)
+R2 <- list.files("/data/jdumbacher/Syma/syma_wgs/syma_dedup",full.names=T) %>% grep("R2.dedup",.,value=T)
 commands <- c()
 sampleID <- c()
 for(i in 1:20){
-  sampleID[i] <- basename(R1[i]) %>% strsplit("_R..dedup") %>% unlist() %>% .[1]
-  commands[i] <- paste0("ngm -r /data/jdumbacher/Syma/syma_wgs/halcyon_senegalensis/B10K-DU-024-03.genomic.fa", #change this to your reference
-                        " -1 ", R1[i], 
-                        " -2 ", R2[i],
-                        " -o ", sampleID[i],".sam",
-                        " -t 48")
+  sampleID[i] <- basename(R1[i]) %>% strsplit("_S...pair1.truncated") %>% unlist() %>% .[1]
+  commands[i] <- paste0("bbmap.sh", 
+                        " in1=", R1[i], 
+                        " in2=", R2[i],
+                        " out=", sampleID[i], ".sam",
+                        " ref=B10K-DU-024-03.genomic.masked.fa", 
+                        " vslow minid=0 k=11 maxindel=200; bamscript=bs.sh; sh bs.sh")
 }
 for(i in 1:20){
   system(commands[i])
 }
 
+<<<<<<< HEAD
+system("mkdir syma_alignment; mv *am syma_alignment")
+=======
 ################################################################
 # sort files                                                   #
 ################################################################
@@ -111,3 +124,26 @@ for(i in 1:length(bams)){
   system(commands[i])
 }
 
+################################################################
+# AddOrReplaceReadGroups                                       #
+################################################################
+bams <- list.files("/data/jdumbacher/Syma/syma_wgs/alignments",full.names=T) %>% grep(".bam$",.,value=T) #$ indicates end of line to avoid .bai files
+commands <- c()
+sampleID <- c()
+for(i in 1:length(bams)){
+  sampleID[i] <- basename(bams[i]) %>% strsplit(".bam") %>% unlist() %>% .[1] 
+  commands[i] <-paste0("picard AddOrReplaceReadGroups",
+                       " I=", bams[i],
+                       " O=", sampleID[i],"_RG.bam",
+                       " R=/data/jdumbacher/Syma/syma_wgs/halcyon_senegalensis/B10K-DU-024-03.genomic.fa",
+                       " RGID=1",
+                       " RGLB=syma",
+                       " RGPL=illumina",
+                       " RGPU=pool1",
+                       " RGSM=",sampleID[i])
+}
+
+for(i in 1:length(bams)){
+  system(commands[i])
+}
+>>>>>>> 1955fde93aea75718a6d3de1f71dbe57da83166d
