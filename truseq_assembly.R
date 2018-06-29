10e70ef40b5e7a43ac9bdcf182f5ccfbe9503435
elinck <elinck@uw.edu>
2018-06-29 13:04:40 -0700

    add contam script, alignment wrapper tweaks

--- truseq_assembly.R
+++ truseq_assembly.R
@@ -7,7 +7,7 @@ setwd("/media/burke/bigMac/ethan/")
 install.packages("magrittr", repo = "http://ftp.osuosl.org/pub/cran/"); install.packages("foreach", repo = "http://ftp.osuosl.org/pub/cran/"); install.packages("doMC", repo = "http://ftp.osuosl.org/pub/cran/")
 library(magrittr);library(foreach)
 
-# setup: you'll need R, java (v1.8), samtools, bbmap
+# setup: you'll need R, java (v1.8), samtools, bbmap, bowtie, picard
 
 # mask repeats from reference using bbmask entropy approach
 system("bbmask.sh in=B10K-DU-024-03.genomic.fa out=masked.ref.fa entropy=0.7")
@@ -19,8 +19,8 @@ commands <- c()
 for(i in 1:length(R1)){
   commands[i] <- paste0("bbduk.sh in1=", R1[i],
                         " in2=",R2[i],
-                        " out1=", R1[i] %>% basename() %>% strsplit(".fastq") %>% unlist() %>% .[1],"trimmed.fq",
-                        " out2=", R2[i] %>% basename() %>% strsplit(".fastq") %>% unlist() %>% .[1],"trimmed.fq",
+                        " out1=", R1[i] %>% basename() %>% strsplit(".fastq") %>% unlist() %>% .[1],".trimmed.fq",
+                        " out2=", R2[i] %>% basename() %>% strsplit(".fastq") %>% unlist() %>% .[1],".trimmed.fq",
                         " ref=adapters.fa ktrim=r k=21 mink=11 hdist=2 tpe tbo")
 }
 for(i in commands){
@@ -29,13 +29,14 @@ for(i in commands){
 system("mkdir syma_trimmed; mv *trimmed* syma_trimmed")
 
 #run bbduk to quality trim
-R1 <- list.files("/data/jdumbacher/Syma/syma_wgs/syma_trimmed",full.names=T) %>% grep("R1_001",.,value=T)
-R2 <- list.files("/data/jdumbacher/Syma/syma_wgs/syma_trimmed",full.names=T) %>% grep("R2_001",.,value=T)
+R1 <- list.files("/media/burke/bigMac/ethan/syma_trimmed",full.names=T) %>% grep("R1",.,value=T)
+R2 <- list.files("/media/burke/bigMac/ethan/syma_trimmed",full.names=T) %>% grep("R2",.,value=T)
 commands <- c()
 for(i in 1:length(R1)){
   commands[i] <- paste0("bbduk.sh in1=", R1[i],
                         " in2=",R2[i],
-                        " out=", R1[i] %>% basename() %>% strsplit("_L006_R._001") %>% unlist() %>% .[1],"clean.fq",
+                        " out1=", R1[i] %>% basename() %>% strsplit(".trimmed.fq") %>% unlist() %>% .[1],".clean.fq",
+                        " out2=", R2[i] %>% basename() %>% strsplit(".trimmed.fq") %>% unlist() %>% .[1],".clean.fq",
                         " qtrim=r trimq=10")
 }
 for(i in commands){
@@ -43,42 +44,43 @@ for(i in commands){
 }
 system("mkdir syma_cleaned; mv *clean* syma_cleaned")
 
-# remove duplicate reads
-R1 <- list.files("/data/jdumbacher/Syma/syma_wgs/syma_cleaned",full.names=T) %>% grep("pair1",.,value=T)
-R2 <- list.files("/data/jdumbacher/Syma/syma_wgs/syma_cleaned",full.names=T) %>% grep("pair2",.,value=T)
-commands <- c()
-for(i in 1:length(R1)){
-  sampleID[i] <- basename(R1[i]) %>% strsplit("_S...pair1.truncated") %>% unlist() %>% .[1]
-  commands[i] <- paste0("dedupe.sh", 
-                        " in1=", R1[i], 
-                        " in2=", R2[i],
-                        " out=", sampleID[i], ".dedup.fq",
-                        " ac=f",
-                        " t=30;",
-                        " reformat.sh in=", sampleID[i], ".dedup.fq", 
-                        " out1=", sampleID[i], "_R1.dedup.fq",
-                        " out2=", sampleID[i], "_R2.dedup.fq")
+# contamination check
+reads <- list.files("/media/burke/bigMac/ethan/syma_cleaned",full.names = T)
+outnames <- reads %>% tools::file_path_sans_ext() %>% tools::file_path_sans_ext() %>% basename()
+for(i in 1:length(reads)){
+  commands[i] <- paste0("echo ",reads[i]," >> contam_alignment_log.txt 2>&1;",
+                        "bowtie2 -p 4",
+                        " -x /media/burke/bigMac/ethan/contaminant_ref_seq/contam_ref",
+                        " -U ",reads[i],
+                        " -S tmp.sam",
+                        " --un ",outnames[i],".filtered.fq",
+                        " >> contam_alignment_log.txt 2>&1;",
+                        
+                        "gzip ",outnames[i],".fq;"
+  )
 }
 for(i in commands){
   system(i)
 }
 
-system("mkdir syma_dedup; mv *dedup* syma_dedup")
+system("rm tmp.sam")
+system("mkdir syma_filtered; mv *filtered* syma_filtered")
 
 # align trimmed reads to reference sample
-R1 <- list.files("/data/jdumbacher/Syma/syma_wgs/syma_dedup",full.names=T) %>% grep("R1.dedup",.,value=T)
-R2 <- list.files("/data/jdumbacher/Syma/syma_wgs/syma_dedup",full.names=T) %>% grep("R2.dedup",.,value=T)
+R1 <- list.files("/data/jdumbacher/Syma/syma_wgs/syma_filtered",full.names=T) %>% grep("R1",.,value=T)
+R2 <- list.files("/data/jdumbacher/Syma/syma_wgs/syma_filtered",full.names=T) %>% grep("R2",.,value=T)
 commands <- c()
 sampleID <- c()
 for(i in 1:length(R1)){
-  sampleID[i] <- basename(R1[i]) %>% strsplit("_S...pair1.truncated") %>% unlist() %>% .[1]
   commands[i] <- paste0("bbmap.sh", 
                         " in1=", R1[i], 
                         " in2=", R2[i],
-                        " out=", sampleID[i], ".sam",
-                        " ref=B10K-DU-024-03.genomic.masked.fa", 
+                        " out=", R1[i] %>% basename() %>% strsplit("R..filtered.fq") %>% unlist() %>% .[1], ".sam",
+                        " ref=ref.masked.fa", 
                         " vslow minid=0 k=11 maxindel=200; bamscript=bs.sh; sh bs.sh")
 }
 for(i in commands){
   system(i)
 }
+
+system("mkdir syma_alignment; mv *am* syma_alignment")
