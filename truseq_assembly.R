9a5aee89808cdab1cff7113686f7356060c163bb
elinck <elinck@uw.edu>
2018-07-16 20:59:11 -0600

    added readgroup bullshit

--- truseq_assembly.R
+++ truseq_assembly.R
@@ -4,7 +4,7 @@
 ############################################################
 
 setwd("/media/burke/bigMac/ethan/")
-install.packages("magrittr", repo = "http://ftp.osuosl.org/pub/cran/"); install.packages("foreach", repo = "http://ftp.osuosl.org/pub/cran/"); install.packages("doMC", repo = "http://ftp.osuosl.org/pub/cran/")
+install.packages("magrittr", repo = "http://ftp.osuosl.org/pub/cran/"); install.packages("foreach", repo = "http://ftp.osuosl.org/pub/cran/"); install.packages("rebus", repo = "http://ftp.osuosl.org/pub/cran/")
 library(magrittr);library(foreach)
 
 ### setup: you'll need R, java (v1.8), samtools, bbmap, bowtie, picard
@@ -59,6 +59,60 @@ for(i in 1:length(R1)){
 }
 for(i in commands){
   system(i)
+}
+
+### fuck I guess I have to use GATK after all
+setwd("/media/burke/bigMac/cj/selasphorus_assembly/")
+# can't figure out a reasonable regex for this because I suck at coding
+bam_wgs <- c("EL1_mega_R1_sorted.bam", "EL10_toro_R1_sorted.bam", "EL11_toro_R1_sorted.bam", "EL13_toro_R1_sorted.bam", "EL18_mega_R1_sorted.bam",
+             "EL19_mega_R1_sorted.bam", "EL20_mega_R1_sorted.bam", "EL21_toro_R1_sorted.bam", "EL23_mega_R1_sorted.bam", "EL24_mega_R1_sorted.bam",
+             "EL27_mega_R1_sorted.bam", "EL29_ochr_R1_sorted.bam", "EL32_toro_R1_sorted.bam", "EL39_toro_R1_sorted.bam", "EL4_mega_R1_sorted.bam",
+             "EL40_toro_R1_sorted.bam", "EL6_mega_R1_sorted.bam", "EL8_toro_R1_sorted.bam", "EL9_toro_R1_sorted.bam", "EL5_ochr_R1_sorted.bam")
+bam_hyrad <- c("EL45_toro_R1_sorted.bam", "EL46_toro_R1_sorted.bam", "EL47_toro_R1_sorted.bam", "EL48_ochr_R1_sorted.bam", "EL49_toro_R1_sorted.bam",
+               "EL41_toro_R1_sorted.bam",  "EL50_toro_R1_sorted.bam", "EL51_toro_R1_sorted.bam", "EL52_toro_R1_sorted.bam", "EL53_toro_R1_sorted.bam",
+               "EL54_toro_R1_sorted.bam", "EL55_toro_R1_sorted.bam", "EL56_toro_R1_sorted.bam", "EL57_toro_R1_sorted.bam", "EL58_toro_R1_sorted.bam",
+               "EL59_toro_R1_sorted.bam", "EL42_toro_R1_sorted.bam",  "EL60_toro_R1_sorted.bam", "EL43_toro_R1_sorted.bam", "EL44_toro_R1_sorted.bam")
+commands <- c()
+for(i in 1:length(bam_wgs)){
+  sampleID <- basename(bam_wgs[i]) %>% strsplit("_R1_sorted.bam") %>% unlist() %>% .[1]
+  commands[i] <- paste0("java -Xmx10g -jar /media/burke/bigMac/Dropbox/tools/picard.jar AddOrReplaceReadGroups",
+                        " I=",bam_wgs[i],
+                        " O=tmp.bam",
+                        " R=/media/burke/bigMac/ethan/masked.ref.fa",
+                        " RGID=1",
+                        " RGLB=syma_wgs",
+                        " RGPL=illumina",
+                        " RGPU=pool1",
+                        " RGSM=",sampleID,";",
+                        
+                        "mv tmp.bam ",bam_wgs[i],";",
+                        
+                        "samtools index ",bam_wgs[i]
+  )
+}
+for(i in 1:length(bam_wgs)){
+  system(commands[i])
+}
+for(i in 1:length(bam_hyrad)){
+  sampleID <- basename(bam_hyrad[i]) %>% strsplit("\\.") %>% unlist() %>% .[1]
+  commands[i] <- paste0("java -Xmx10g -jar /media/burke/bigMac/Dropbox/tools/picard.jar AddOrReplaceReadGroups",
+                        " I=",bam_hyrad[i],
+                        " O=tmp.bam",
+                        " R=/media/burke/bigMac/ethan/masked.ref.fa",
+                        " RGID=2",
+                        " RGLB=syma_hyrad",
+                        " RGPL=illumina",
+                        " RGPU=pool2",
+                        " RGSM=",sampleID,";",
+                        
+                        "mv tmp.bam ",bam_hyrad[i],";",
+                        
+                        "samtools index ",bam_hyrad[i]
+  )
+}
+
+for(i in 1:length(bam_hyrad)){
+  system(commands[i])
 }
 
 ### vcftools commands
